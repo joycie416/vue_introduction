d68e202c5b6f7824391792529ae01f6a7ff57937
joycie416 <joycie416@gmail.com>
2026-04-24 17:55:01 +0900

    03-01: 데이터 정의와 데이터 보간법

---
+++ src/components/Test.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      library: "Vue.js",
+      content: "데이터 정의와 데이터 보간법",
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>현재 사용중인 라이브러리 : {{ library }}</h1>
+    <h2>기초 문법 이해하기 1 : {{ content }}</h2>
+  </div>
+</template>
+
+<style scoped></style>
